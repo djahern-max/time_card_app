7bd95ea8503bd1dfffab11ca4ad0280717b25313
Dane Ahern <danielaherniv@gmail.com>
2024-08-21 08:44:19 -0400

    Scrollable Schedule

--- frontend/src/components/Schedule/EmployeeSchedule.js
+++ frontend/src/components/Schedule/EmployeeSchedule.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./EmployeeSchedule.css";
 
 function EmployeeSchedule() {
   const { empCode } = useParams();
